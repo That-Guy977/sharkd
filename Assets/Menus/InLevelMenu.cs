d37f620fb6c2f474ba33b2483fbd48186c7a0282
That_Guy977 <sirawit.andy977@gmail.com>
2025-04-28 14:18:18 +0700

    fix: player input while paused

--- Assets/Menus/InLevelMenu.cs
+++ Assets/Menus/InLevelMenu.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Eflatun.SceneReference;
 
 class InLevelMenu : MonoBehaviour {
+    public PlayerInput playerInput;
     public SceneReference mainMenu;
     public SceneLoader.Transition restartTransition;
     public SceneLoader.Transition exitTransition;
 
     void OnEnable() {
         Time.timeScale = 0;
+        playerInput.enabled = false;
         AudioListener.pause = true;
         MusicPlayer.instance.Pause();
     }
 
     void OnDisable() {
         Time.timeScale = 1;
+        playerInput.enabled = true;
         AudioListener.pause = false;
         MusicPlayer.instance.Unpause();
     }
