fe80e7ec52f1f33ba4a5facac290dddfce7a6f0f
That_Guy977 <sirawit.andy977@gmail.com>
2025-05-26 22:46:59 +0700

    chore: clean up unused script

--- Assets/Levels/GawrLevel/Gawr/GawrEnemySlashTarget.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-class GawrEnemySlashTarget : MonoBehaviour {
-    public GawrAttack attack;
-
-    void OnTriggerEnter2D(Collider2D collider) {
-        attack.Target(collider);
-    }
-}
